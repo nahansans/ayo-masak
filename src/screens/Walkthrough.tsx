a65d9d683444ba0fbe3aa369eed7bb3d76b6639a
Naufal Hanif <nahanifg@gmail.com>
2020-12-04 09:46:21 +0700

    change walkthrough

--- src/screens/Walkthrough.tsx
+++ src/screens/Walkthrough.tsx
@@ -5,6 +5,8 @@ import { StackNavigationProp } from '@react-navigation/stack'
 import { RouteProp } from '@react-navigation/native'
 import { NavigationType } from '../types/navigation'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import LinearGradient from 'react-native-linear-gradient'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 
 type PropsList = {
@@ -16,17 +18,17 @@ const DATA = [
     {
         key: 0,
         image: `https://images.unsplash.com/photo-1506368249639-73a05d6f6488?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80`,
-        description: `Find the recipes for your breakfast to start a perfect day.`
+        description: `Temukan resep untuk sarapan Anda untuk memulai hari yang sempurna.`
     },
     {
         key: 1,
         image: `https://images.unsplash.com/photo-1547592180-85f173990554?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80`,
-        description: `Hang out with food lovers around you, invite them for a taste.`
+        description: `Bergaul dengan pecinta makanan di sekitar Anda, undang mereka untuk mencicipi.`
     },
     {
         key: 2,
         image: `https://images.unsplash.com/photo-1528712306091-ed0763094c98?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=380&q=80`,
-        description: `Discover all the cooking recipes from around the world.`
+        description: `Temukan semua resep memasak dari seluruh dunia.`
     }
 ]
 
@@ -49,7 +51,7 @@ const Indicator = ({ScrollX}: any) => {
                             height: 5,
                             width: 5,
                             borderRadius: 5,
-                            backgroundColor: `#333`,
+                            backgroundColor: `#303030`,
                             margin: 5,
                             transform: [{scale}]
                         }}
@@ -66,6 +68,14 @@ const Walkthrough = (props: PropsList) => {
     const ScrollX = useRef(new Animated.Value(0)).current
     return (
         <View style = {{ flex: 1, alignItems: `center` }} >
+            <LinearGradient
+                colors = {["white", "white","paleturquoise"]}
+                style = {{
+                    position: "absolute",
+                    top: 0, left: 0, bottom: 0, right: 0,
+                    // opacity: 0.8
+                }}
+            />
             <Animated.FlatList
                 data = {DATA}
                 keyExtractor = {(item) => String(item.key)}
@@ -102,6 +112,17 @@ const Walkthrough = (props: PropsList) => {
                             0, 1, 0
                         ]
                     })
+
+                    const opacityImage = ScrollX.interpolate({
+                        inputRange: [
+                            (index - 0.5) * width,
+                            index * width,
+                            (index + 0.5) * width
+                        ],
+                        outputRange: [
+                            0, 1, 0
+                        ]
+                    })
                     return (
                         <>
                             <View style = {{ width, alignItems: `center` }} >
@@ -122,7 +143,7 @@ const Walkthrough = (props: PropsList) => {
                                                 height: height / 2,
                                                 // borderRadius: height,
                                                 transform: [{ translateX: translateXImage }],
-                                                opacity
+                                                opacity: opacityImage
                                             }}
                                         />
                                     </Animated.View>
@@ -133,7 +154,8 @@ const Walkthrough = (props: PropsList) => {
                                             textAlign: `center`,
                                             padding: 20,
                                             transform: [{ translateX }],
-                                            opacity
+                                            opacity,
+                                            color: "#303030"
                                         }}
                                     >
                                         {item.description}
@@ -150,10 +172,10 @@ const Walkthrough = (props: PropsList) => {
                 style = {{
                     paddingHorizontal: 20,
                     paddingVertical: 10,
-                    borderWidth: 1,
+                    borderWidth: 2,
                     borderColor: `#303030`,
                     margin: 10,
-                    marginBottom: 20,
+                    marginBottom: 35,
                     borderRadius: 8
                 }}
                 onPress = {async() => {
@@ -163,7 +185,7 @@ const Walkthrough = (props: PropsList) => {
             >
                 <Text
                     style = {{
-                        fontFamily: Fonts.Lato.Regular,
+                        fontFamily: Fonts.Lato.Bold,
                         color: `#303030`,
                         fontSize: 16
                     }}
